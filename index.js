3d8e695a88b6adac0960afb5423f8be6d8d37ba4
vmunozre <vicmunozr@gmail.com>
2025-01-23 16:33:15 +0000

    Added types

--- index.js
+++ index.js
@@ -4,7 +4,23 @@
  * - MIT License
  * - 2025
  */
+
+/**
+ * Options to configure the CaosJS instance.
+ * @typedef {Object} CaosJSOptions
+ * @property {string|string[]} [selectors=[]] - Selectors to apply the effect to. Can be a single string or an array of strings.
+ * @property {number} [maxRotation=2] - Maximum degrees of rotation.
+ * @property {number} [minRotation=-2] - Minimum degrees of rotation.
+ * @property {boolean} [allowFrequency=false] - Whether to allow frequency to determine effect application.
+ * @property {boolean} [applyFrequencyToAllElements=false] - Whether to apply frequency to all elements.
+ * @property {number} [frequency=0.5] - The frequency of the effect (0 to 1).
+ * @property {number} [delay=0] - Delay in milliseconds before applying the effect after initialization.
+ */
 class CaosJS {
+  /**
+   * Creates a new instance of CaosJS.
+   * @param {CaosJSOptions} [options={}] - Configuration options for the instance.
+   */
   constructor(options = {}) {
     const { selectors } = options
     if (typeof selectors === "string") {
@@ -29,6 +45,9 @@ class CaosJS {
     }, this.config.delay)
   }
 
+  /**
+   * Initializes the effect, applying it based on the configuration.
+   */
   init() {
     if (this.config.allowFrequency) {
       if (Math.random() < this.config.frequency) {
@@ -39,6 +58,9 @@ class CaosJS {
     }
   }
 
+  /**
+   * Applies the effect to the configured selectors and elements.
+   */
   applyEffect() {
     this.config.selectors.forEach(selector => {
       const elements = document.querySelectorAll(selector)
@@ -54,11 +76,19 @@ class CaosJS {
     })
   }
 
+  /**
+   * Applies a random rotation effect to a specific HTML element.
+   * @param {HTMLElement} element - The HTML element to caosify.
+   */
   caosifyElement(element) {
     const rotation = this.getRandomRotation()
     element.style.transform = `rotate(${rotation}deg)`
   }
 
+  /**
+   * Gets a random rotation value within the configured range.
+   * @returns {number} A random number between `minRotation` and `maxRotation`.
+   */
   getRandomRotation() {
     const { minRotation, maxRotation } = this.config
     return Math.random() * (maxRotation - minRotation) + minRotation
